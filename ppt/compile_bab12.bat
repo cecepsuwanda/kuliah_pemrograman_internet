7b9a29c3cd8d8c3c4c45b7e9d5f5acde980fe21b
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2026-03-11 03:20:54 +0700

    ok
    ok

---
+++ ppt/compile_bab12.bat
@@ -0,0 +1,30 @@
+@echo off
+chcp 65001 >nul
+setlocal
+
+set TEXFILE=Bab12_PHP_Dasar_Pemrosesan_Form.tex
+set BASENAME=Bab12_PHP_Dasar_Pemrosesan_Form
+
+cd /d "%~dp0"
+
+echo [1/3] Kompilasi pertama...
+pdflatex -interaction=nonstopmode "%TEXFILE%" >nul 2>&1
+
+echo [2/3] Kompilasi kedua (untuk referensi)...
+pdflatex -interaction=nonstopmode "%TEXFILE%" >nul 2>&1
+
+echo [3/3] Membersihkan file sementara (kecuali .log, .out, dan .pdf)...
+
+if exist "%BASENAME%.aux" del "%BASENAME%.aux"
+if exist "%BASENAME%.toc" del "%BASENAME%.toc"
+if exist "%BASENAME%.lof" del "%BASENAME%.lof"
+if exist "%BASENAME%.lot" del "%BASENAME%.lot"
+if exist "%BASENAME%.fls" del "%BASENAME%.fls"
+if exist "%BASENAME%.fdb_latexmk" del "%BASENAME%.fdb_latexmk"
+if exist "%BASENAME%.synctex.gz" del "%BASENAME%.synctex.gz"
+if exist "%BASENAME%.nav" del "%BASENAME%.nav"
+if exist "%BASENAME%.snm" del "%BASENAME%.snm"
+
+echo.
+echo Selesai. Hasil: %BASENAME%.pdf
+pause
